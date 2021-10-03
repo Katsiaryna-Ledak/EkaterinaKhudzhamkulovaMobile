c221c55c9cec3458a0631af39e21f7546a07de07
Katsiaryna-Ledak <ekaterina_ledak@gmail.com>
2021-10-03 21:02:58 +0300

    ready to merge

--- src/main/java/utils/TestProperties.java
+++ src/main/java/utils/TestProperties.java
@@ -8,10 +8,8 @@ import java.util.Properties;
 public class TestProperties {
     private static Properties testNativeDataProperties;
     private static Properties testWebDataProperties;
-    private static Properties wrongDataProperties;
     private static String NativeDataPropsPath = "src/main/resources/testnative.properties";
     private static String WebDataPropsPath = "src/main/resources/testweb.properties";
-    private static String WrongDataPropsPath = "src/main/resources/wrongtestdata.properties";
 
     public static Properties getTestNativeDataProperties() {
         testNativeDataProperties = new Properties();
@@ -33,16 +31,5 @@ public class TestProperties {
             ex.printStackTrace();
         }
         return testWebDataProperties;
-    }
-
-    public static Properties getWrongDataProperties() {
-        wrongDataProperties = new Properties();
-        try (InputStream in = new FileInputStream(WrongDataPropsPath)) {
-            wrongDataProperties.load(in);
-        } catch (IOException ex) {
-            System.out.println("Can not load properties file");
-            ex.printStackTrace();
-        }
-        return wrongDataProperties;
     }
 }
